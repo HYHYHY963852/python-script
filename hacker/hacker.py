1f7fb7bf827feb0e87a15bf93ee9eebfd9f83bb4
29586 <295868783@qq.com>
2023-06-05 16:01:32 +0800

    黑客雨

---
+++ hacker/hacker.py
@@ -0,0 +1,5 @@
+import colorama
+#!/usr/bin/Python3
+from colorama import Fore, Back, Style
+print (Fore.CYAN + "Hello World!\n")
+print (Fore.GREEN + "My first Script")
---
+++ 测试.py
@@ -0,0 +1,9 @@
+temp = input("猜猜我心里想的什么")
+guess = int(temp)
+
+if guess ==8:
+    print("你是我心里的小蛔虫嘛")
+    print("猜中了也没有")
+else:
+    print("猜错了")
+print("游戏结束，不玩了")
